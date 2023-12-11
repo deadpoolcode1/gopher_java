b044e1b11c6170e423f0a23f7fba3715e3477e27
Ilan Ganor <ilan@kamacode.com>
2023-12-11 06:00:31 +0200

    database logic works as expected

--- src/main/java/com/yourcompany/app/Monitor.java
+++ src/main/java/com/yourcompany/app/Monitor.java
@@ -10,27 +10,35 @@ public class Monitor {
 
     private static void processEntry(String entry, String action, String server, String username, String password, String dbName) {
         System.out.println("Action " + action + " was requested, table row is: " + entry);
-
-        String id = entry.split(",")[0]; // Assuming 'id' is the first value in a comma-separated entry
-
+    
+        // Extract the 'id' from the entry, assuming it's the first value in a comma-separated entry
+        String id = entry.split(",")[0];
+    
+        // Update query to set request_pending to 0 and timestamp_pending_process to the current timestamp
         String updateQuery1 = "UPDATE " + (action.equals("read") ? "read_data" : "write_data") +
                               " SET request_pending = 0, timestamp_pending_process = CURRENT_TIMESTAMP WHERE id = " + id;
-
+    
         Database.executeNonQuery(dbName, updateQuery1, server, username, password);
-
-        System.out.println("Processing LMDS start");
-
+    
+        System.out.println("Processing LMDS start ID: " + id);
+    
         if (action.equals("read")) {
+            // Update query to set reply_pending to 1
             String updateQuery2 = "UPDATE read_data SET reply_pending = 1 WHERE id = " + id;
             Database.executeNonQuery(dbName, updateQuery2, server, username, password);
-
+    
             boolean stopRequested = false;
             while (!stopRequested) {
+                // Query to check if the stop request is made
                 String checkStopQuery = "SELECT request_to_stop FROM read_data WHERE id = " + id;
                 List<String> result = Database.executeQuery(dbName, checkStopQuery, server, username, password);
-                if (!result.isEmpty() && result.get(0).equals("1")) {
+                
+    
+                // Check if the stop request is made
+                if (!result.isEmpty() && "1".equals(result.get(0))) {
                     stopRequested = true;
                 } else {
+                    // Insert 'hello' into read_data_info and sleep for 1 second
                     String insertQuery = "INSERT INTO read_data_info (read_data_id, data) VALUES (" + id + ", 'hello')";
                     Database.executeNonQuery(dbName, insertQuery, server, username, password);
                     try {
@@ -42,9 +50,11 @@ public class Monitor {
                 }
             }
         }
-
-        System.out.println("Processing LMDS end");
+    
+        System.out.println("Processing LMDS end ID: " + id);
     }
+    
+    
 
     public static boolean monitor() {
         String server = Config.getDBServer();
@@ -58,7 +68,7 @@ public class Monitor {
             return false;
         }
 
-        System.out.println("Monitoring started.");
+        System.out.println("Monitoring started now.");
 
         ExecutorService executor = Executors.newCachedThreadPool();
 
