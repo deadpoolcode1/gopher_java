cd3f8012ca0abed148e871efb0d30b096d1c3544
ilan@kamacode.com <ilan ganor>
2024-02-07 11:34:08 +0200

    now passing the data

--- src/main/java/ATE_MAIN/main.java
+++ src/main/java/ATE_MAIN/main.java
@@ -281,7 +281,7 @@ public class main {
         EnDef.process_name_ce destProcessName = EnDef.process_name_ce.valueOf(jsonObj.getAsJsonObject("LMH").getAsJsonObject("dest_address").get("process_name").getAsString());
         EnDef.host_name_ce sendHostName = EnDef.host_name_ce.valueOf(jsonObj.getAsJsonObject("LMH").getAsJsonObject("sender_address").get("host_name").getAsString());
         EnDef.process_name_ce sendProcessName = EnDef.process_name_ce.valueOf(jsonObj.getAsJsonObject("LMH").getAsJsonObject("sender_address").get("process_name").getAsString());
-        EnDef.msg_code_ce msgCode =  EnDef.msg_code_ce.valueOf(jsonObj.getAsJsonObject("LMH").get("msg_code").getAsString());
+        EnDef.msg_code_ce msgCode = EnDef.msg_code_ce.valueOf(jsonObj.get("msg_code").getAsString());
         //EnDef.msg_code_ce msgCode = EnDef.msg_code_ce.valueOf(jsonObj.get("msg_code").getAsString());
     
         Address destAddress = new Address();
